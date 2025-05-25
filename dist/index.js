cc819268dcea2afb9f5a729ad2d124f99706049b
deepan <deepanappu1999@gmail.com>
2025-05-25 21:48:39 +0530

    ts config file

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,16 @@
 "use strict";
-// incase your getting some error in while debugging 
-// AppData\Roaming\npm\tsc.ps1 cannot be loaded because running scripts is disabled on this system. For 
-// more information, see about_Execution_Policies at https:/go.microsoft.com/fwlink/?LinkID=135170.
-// At line:1 char:1
-// Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-// reverting to pervious changes
-// Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Undefined
-let age = 20;
-if (age < 50) {
-    age += 10;
-    console.log(age);
+class Employee {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
+class Person extends Employee {
+    constructor(firstName, lastName, salary) {
+        super(firstName, lastName);
+        this.salary = salary;
+    }
+}
+let personObj = new Person('jimmy', 'anderson', 20000);
+console.log(personObj.salary);
 //# sourceMappingURL=index.js.map
